3ed606e0d46331c696633cdd1e9c5ed57fa9d993
p-morris <pete@iosfaststart.com>
2018-11-19 17:44:20 +0800

    Initial commit

---
+++ LightingKit/LightingKit/LightingKit.h
@@ -0,0 +1,19 @@
+//
+//  LightingKit.h
+//  LightingKit
+//
+//  Created by Peter Morris on 19/11/2018.
+//  Copyright © 2018 Pete Morris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightingKit.
+FOUNDATION_EXPORT double LightingKitVersionNumber;
+
+//! Project version string for LightingKit.
+FOUNDATION_EXPORT const unsigned char LightingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightingKit/PublicHeader.h>
+
+
