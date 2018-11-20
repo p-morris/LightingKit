5bf6a2d4562184e67f2b7c81f0d589a824d80ad9
p-morris <pete@iosfaststart.com>
2018-11-20 17:58:35 +0800

    added example project

---
+++ Example/LightingKit Example/LightingKit Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightingKit Example
+//
+//  Created by Peter Morris on 20/11/2018.
+//  Copyright © 2018 Pete Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
