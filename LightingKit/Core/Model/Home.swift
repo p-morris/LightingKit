e8b52e86cf88f5bb21ab276745e71d0d509dd1a1
p-morris <pete@iosfaststart.com>
2018-11-24 00:05:58 +0800

    Added documentation

--- LightingKit/Core/Model/Home.swift
+++ LightingKit/Core/Model/Home.swift
@@ -9,9 +9,19 @@
 import Foundation
 import HomeKit
 
+/// Represents a HomeKit home.
 public struct Home: LightingKitObject {
+    /// The name of the home.
     public let name: String
+    /// The unique identifier of the home.
     public let id: UUID
+    /**
+     Initializes a `Home` object.
+     - Parameters:
+     - name: The name of the light.
+     - id: The unique identifier for the light.
+     - returns: An initialized `Home` object.
+     */
     public init(name: String, id: UUID) {
         self.name = name
         self.id = id
--- LightingKit/Core/Model/Room.swift
+++ LightingKit/Core/Model/Room.swift
@@ -9,9 +9,19 @@
 import Foundation
 import HomeKit
 
+/// Represents a HomeKit room
 public struct Room: LightingKitObject {
+    /// The name of the room.
     public let name: String
+    /// The unique identifier for the room.
     public let id: UUID
+    /**
+     Initializes a `Room` object.
+     - Parameters:
+     - name: The name of the light.
+     - id: The unique identifier for the light.
+     - returns: An initialized `Room` object.
+     */
     public init(name: String, id: UUID) {
         self.name = name
         self.id = id
